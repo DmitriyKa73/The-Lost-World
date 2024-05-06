32e0bf95547d89e6bfcaf67cf0af6ef9cecba1cc
DmitriyKa73 <109173727+DmitriyKa73@users.noreply.github.com>
2024-05-06 21:52:27 +0400

    fix
    Skifcha

--- game_window.py
+++ game_window.py
@@ -11,35 +11,40 @@ class GameWindow:
         self.label = tk.Label(root, text="Добро пожаловать в игру!", font=("Arial", 30))
         self.label.pack(pady=20)
 
-        self.start_button = ttk.Button(root, text="Начать игру", style="Game.TButton", command=self.start_game)
-        self.start_button.pack(pady=20, padx=40)
+        self.button_frame = tk.Frame(root)
+        self.button_frame.pack(expand=True)  # Растягиваем фрейм по обе стороны
 
-        self.about_button = ttk.Button(root, text="О проекте", style="Game.TButton", command=self.show_about)
-        self.about_button.pack(pady=20, padx=40)
+        style = ttk.Style()
+        style.configure("Game.TButton", font=("Arial", 20), width=15, padding=10)
 
-        self.quit_button = ttk.Button(root, text="Выйти из игры", style="Game.TButton", command=self.quit_game)
-        self.quit_button.pack(pady=20, padx=40)
+        self.start_button = ttk.Button(self.button_frame, text="Начать игру", style="Game.TButton", command=self.start_game)
+        self.start_button.pack(pady=10)  # Увеличиваем отступ
+
+        self.about_button = ttk.Button(self.button_frame, text="О проекте", style="Game.TButton", command=self.show_about)
+        self.about_button.pack(pady=10)  # Увеличиваем отступ
+
+        self.quit_button = ttk.Button(self.button_frame, text="Выйти из игры", style="Game.TButton", command=self.quit_game)
+        self.quit_button.pack(pady=10)  # Увеличиваем отступ
 
     def center_window(self):
         window_width = self.root.winfo_reqwidth()
         window_height = self.root.winfo_reqheight()
         screen_width = self.root.winfo_screenwidth()
         screen_height = self.root.winfo_screenheight()
-        x = int((screen_width - 800) / 2)  # Установим ширину окна в 800
-        y = int((screen_height - 600) / 2)  # Установим высоту окна в 600
-        self.root.geometry(f"800x600+{x}+{y}")  # Установим размеры окна и его положение
+        x = int((screen_width - 800) / 2)
+        y = int((screen_height - 600) / 2)
+        self.root.geometry(f"800x600+{x}+{y}")
 
     def show(self):
-        self.root.deiconify()  # Показать окно игры
+        self.root.deiconify()
 
     def hide(self):
-        self.root.withdraw()  # Скрыть окно игры
+        self.root.withdraw()
+
     def start_game(self):
-        # Логика для начала игры
         pass
 
     def show_about(self):
-        # Логика для показа информации о проекте
         pass
 
     def quit_game(self):
